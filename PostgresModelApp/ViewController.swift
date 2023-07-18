4ba8e4262647576445b191ee0d38b1a71e14d24c
kelvinjjwong <kelvinjjwong@outlook.com>
2023-07-19 07:24:32 +0800

    Initial Commit

---
+++ PostgresModelApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PostgresModelApp
+//
+//  Created by kelvinwong on 2023/7/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
